5197c6f7ee7f5ae688fbbc48e015351cfd57172e
golbanv <golbanvladislav@gmail.com>
2020-07-20 21:41:46 +0200

    Add volcanos coordinates

--- map1.py
+++ map1.py
@@ -1,9 +1,15 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2,-99.1],[39.2,-97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a marker", icon=folium.Icon(color="green")))
+for lt,  ln, el in zip(lat,lon, elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(el)+" meters", parse_html=True), icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 map.save("Map1.html")
