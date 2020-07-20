7ac88dc28930d057916b88784501bf0fec33f56c
golbanv <golbanvladislav@gmail.com>
2020-07-20 22:09:40 +0200

    Add population layer

--- map1.py
+++ map1.py
@@ -20,5 +20,8 @@ fg = folium.FeatureGroup(name="My Map")
 for lt,  ln, el in zip(lat,lon, elev):
     fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(el)+" meters", parse_html=True), icon=folium.Icon(color=color_producer(el))))
 
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"]< 10000000
+else "orange" if 10000000 <= x["properties"]["POP2005"]<50000000 else "red"}))
 map.add_child(fg)
 map.save("Map1.html")
